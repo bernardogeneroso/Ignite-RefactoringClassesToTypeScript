98b630124e36e9f7413b55611ea4b50ab1492eb1
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-21 18:36:00 +0100

    Challenge completed

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { FoodInfo } from '../Food';
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  handleUpdateFood(data: Omit<FoodInfo, 'id' | 'available'>): void;
+  setIsOpen(): void;
+  editingFood: FoodInfo | undefined;
+}
+
+interface FormData {
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+}
+
+const ModalEditFood: React.FC<ModalEditFoodProps> = ({editingFood, handleUpdateFood, isOpen, setIsOpen}) => {
+  const handleSubmit = async (data: FormData) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalEditFood;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState,  useEffect } from 'react';
+
+import Header from '../../components/Header';
+import api from '../../services/api';
+import Food, { FoodInfo } from '../../components/Food';
+import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
+import { FoodsContainer } from './styles';
+
+const Dashboard: React.FC = () => {
+  const [foods, setFoods] = useState<FoodInfo[]>([])
+  const [editingFood, setEditingFood] = useState<FoodInfo | undefined>({} as FoodInfo)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [editModalOpen, setEditModalOpen] = useState(false)
+
+  useEffect(() => {
+    const loadFoods = async () => {
+      const {data} = await api.get("/foods")
+
+      setFoods(data)
+    }
+
+    loadFoods()
+  }, [])
+
+  const handleAddFood = async (food: FoodInfo) => {
+    try {
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleUpdateFood = async (food: FoodInfo) => {
+    try {
+      if (!editingFood) throw new Error()
+
+      const foodUpdated = await api.put(
+        `/foods/${editingFood.id}`,
+        { ...editingFood, ...food },
+      );
+
+      const foodsUpdated = foods.map(f =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleDeleteFood = async (id: number) => {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter(food => food.id !== id);
+
+    setFoods(foodsFiltered);
+  }
+
+  const toggleModal = () => {
+    setModalOpen(state => !state);
+  }
+
+  const toggleEditModal = () => {
+    setEditModalOpen(state => !state);
+  }
+
+  const handleEditFood = (food: FoodInfo) => {
+    setEditingFood(food)
+    setEditModalOpen(true)
+  }
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map(food => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+};
+
+export default Dashboard;
